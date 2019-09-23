94f788c61b4ba8a95984f0debbecdb043637644b
Mateus Viegas <mateuscviegas@gmail.com>
2019-09-23 23:34:47 +0100

    Exemplo Rabbit

--- RabbitMQDemo.Consumer/Program.cs
+++ RabbitMQDemo.Consumer/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using RabbitMQ.Client;
 
 namespace RabbitMQDemo.Consumer
 {
-    class Program
+    public class Program
     {
+        private static IConfiguration _configuration;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json");
+            _configuration = builder.Build();
+
+            // Para este exemplo foi criado um container Docker baseado
+            // em uma imagem do RabbitMQ. Segue o comando para geração
+            // desta estrutura:
+            // docker run -d --hostname rabbit-local --name testes-rabbitmq -p 6672:5672 -p 15672:15672 -e RABBITMQ_DEFAULT_USER=testes -e RABBITMQ_DEFAULT_PASS=Testes2018! rabbitmq:3-management-alpine
+            var rabbitMQConfigurations = new RabbitMQConfigurations();
+            new ConfigureFromConfigurationOptions<RabbitMQConfigurations>(
+                _configuration.GetSection("RabbitMQConfigurations"))
+                    .Configure(rabbitMQConfigurations);
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = rabbitMQConfigurations.HostName,
+                Port = rabbitMQConfigurations.Port,
+                UserName = rabbitMQConfigurations.UserName,
+                Password = rabbitMQConfigurations.Password
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "TestesASPNETCore",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+                channel.BasicConsume(queue: "TestesASPNETCore",
+                     autoAck: true,
+                     consumer: consumer);
+
+                Console.WriteLine("Aguardando mensagens para processamento");
+                Console.WriteLine("Pressione uma tecla para encerrar...");
+                Console.ReadKey();
+            }
+        }
+
+
+        private static void Consumer_Received(
+            object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine(Environment.NewLine +
+                "[Nova mensagem recebida] " + message);
         }
     }
 }
--- RabbitMQDemo.Publisher/Program.cs
+++ RabbitMQDemo.Publisher/Program.cs
@@ -6,7 +6,33 @@ namespace RabbitMQDemo.Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory()
+                {
+                    HostName = configurations.HostName,
+                    Port = configurations.Port,
+                    UserName = configurations.UserName,
+                    Password = configurations.Password
+                };
+
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "TestesASPNETCore",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message =
+                        $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - " +
+                        $"Conteúdo da Mensagem: {conteudo.Mensagem}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "TestesASPNETCore",
+                                         basicProperties: null,
+                                         body: body);
+                }
         }
     }
 }
