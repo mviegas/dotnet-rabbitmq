37ed0da9ef6464473dda419703a4decc174e70e3
Mateus Viegas <mateuscviegas@gmail.com>
2019-12-15 23:09:45 +0000

    Ajustes no Pub/Sub

--- RabbitMQDemo.Consumer/Program.cs
+++ RabbitMQDemo.Consumer/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMQDemo.Consumer
 {
@@ -9,29 +14,16 @@ namespace RabbitMQDemo.Consumer
 
         static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile($"appsettings.json");
-            _configuration = builder.Build();
-
-            // Para este exemplo foi criado um container Docker baseado
-            // em uma imagem do RabbitMQ. Segue o comando para geração
-            // desta estrutura:
-            // docker run -d --hostname rabbit-local --name testes-rabbitmq -p 6672:5672 -p 15672:15672 -e RABBITMQ_DEFAULT_USER=testes -e RABBITMQ_DEFAULT_PASS=Testes2018! rabbitmq:3-management-alpine
-            var rabbitMQConfigurations = new RabbitMQConfigurations();
-            new ConfigureFromConfigurationOptions<RabbitMQConfigurations>(
-                _configuration.GetSection("RabbitMQConfigurations"))
-                    .Configure(rabbitMQConfigurations);
-
-            var factory = new ConnectionFactory()
+            var connectionFactory = new ConnectionFactory()
             {
-                HostName = rabbitMQConfigurations.HostName,
-                Port = rabbitMQConfigurations.Port,
-                UserName = rabbitMQConfigurations.UserName,
-                Password = rabbitMQConfigurations.Password
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "tangaras.cmflex.com.br"
             };
 
-            using (var connection = factory.CreateConnection())
+            using (var connection = connectionFactory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
                 channel.QueueDeclare(queue: "TestesASPNETCore",
@@ -41,9 +33,18 @@ namespace RabbitMQDemo.Consumer
                                      arguments: null);
 
                 var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += Consumer_Received;
+
+                consumer.Received += (sender, eventArgs) =>
+                {
+                    var message = Encoding.UTF8.GetString(eventArgs.Body);
+
+                    Console.WriteLine(Environment.NewLine + "[Nova mensagem recebida] " + message);
+
+                    channel.BasicAck(eventArgs.DeliveryTag, false);
+                };
+
                 channel.BasicConsume(queue: "TestesASPNETCore",
-                     autoAck: true,
+                     autoAck: false,
                      consumer: consumer);
 
                 Console.WriteLine("Aguardando mensagens para processamento");
@@ -51,14 +52,17 @@ namespace RabbitMQDemo.Consumer
                 Console.ReadKey();
             }
         }
+    }
 
-
-        private static void Consumer_Received(
-            object sender, BasicDeliverEventArgs e)
+    internal class RabbitMQConfigurations
+    {
+        public RabbitMQConfigurations()
         {
-            var message = Encoding.UTF8.GetString(e.Body);
-            Console.WriteLine(Environment.NewLine +
-                "[Nova mensagem recebida] " + message);
         }
+
+        public string HostName { get; internal set; }
+        public int Port { get; internal set; }
+        public string UserName { get; internal set; }
+        public string Password { get; internal set; }
     }
 }
--- RabbitMQDemo.Publisher/Program.cs
+++ RabbitMQDemo.Publisher/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace RabbitMQDemo.Publisher
 {
@@ -6,17 +10,27 @@ namespace RabbitMQDemo.Publisher
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory()
-                {
-                    HostName = configurations.HostName,
-                    Port = configurations.Port,
-                    UserName = configurations.UserName,
-                    Password = configurations.Password
-                };
+            string vhost = AskForVHost();
 
-                using (var connection = factory.CreateConnection())
-                using (var channel = connection.CreateModel())
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = vhost
+            };
+
+            EnsureThatVHostExists(vhost, connectionFactory);
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                while (true)
                 {
+                    Console.WriteLine("Type your message");
+                    var teste = Console.ReadLine();
+
                     channel.QueueDeclare(queue: "TestesASPNETCore",
                                          durable: false,
                                          exclusive: false,
@@ -25,7 +39,7 @@ namespace RabbitMQDemo.Publisher
 
                     string message =
                         $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - " +
-                        $"Conteúdo da Mensagem: {conteudo.Mensagem}";
+                        $"Conteúdo da Mensagem: {teste}";
                     var body = Encoding.UTF8.GetBytes(message);
 
                     channel.BasicPublish(exchange: "",
@@ -33,6 +47,30 @@ namespace RabbitMQDemo.Publisher
                                          basicProperties: null,
                                          body: body);
                 }
+            }
+        }
+
+        private static string AskForVHost()
+        {
+            Console.WriteLine("Call VHOST");
+            var vhost = Console.ReadLine();
+            return vhost;
+        }
+
+        private static void EnsureThatVHostExists(string vhost, ConnectionFactory factory)
+        {
+            var credentials = new NetworkCredential() { UserName = factory.UserName, Password = factory.Password };
+            using (var handler = new HttpClientHandler { Credentials = credentials })
+            using (var client = new HttpClient(handler))
+            {
+                var url = $"http://{factory.HostName}:15672/api/vhosts/{vhost}";
+
+                var content = new StringContent("", Encoding.UTF8, "application/json");
+                var result = client.PutAsync(url, content).Result;
+
+                if ((int)result.StatusCode >= 300)
+                    throw new Exception(result.ToString());
+            }
         }
     }
 }
