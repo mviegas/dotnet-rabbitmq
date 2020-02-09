a10086860b2a70120c9167e6d21d0c4fd5536803
Mateus Viegas <mateuscviegas@gmail.com>
2020-02-09 11:42:57 +0000

    Added connectionString parameter to native DI extension

--- src/Core/RabbitSharp/DI/Registration.cs
+++ src/Core/RabbitSharp/DI/Registration.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 
 namespace SharpRabbit
 {
     public static class Registration
     {
-        public static IServiceCollection AddSharpRabbit(this IServiceCollection owner)
+        public static IServiceCollection AddSharpRabbit(this IServiceCollection owner, string connectionString)
         {
-            owner.AddSingleton<IRabbitConnection, RabbitConnection>();
+            owner.AddSingleton<IRabbitConnection>(serviceProvider =>
+            {
+                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+                return new RabbitConnection(
+                    loggerFactory,
+                    connectionFactory: new ConnectionFactory()
+                    {
+                        Endpoint = new AmqpTcpEndpoint(connectionString)
+                    });
+            });
 
             return owner;
         }
