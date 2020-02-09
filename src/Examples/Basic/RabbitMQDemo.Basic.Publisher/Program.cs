ff0647e6e91c8b0ccb7c26f825411afce71427a1
Mateus Viegas <mateuscviegas@gmail.com>
2020-02-09 11:19:24 +0000

    Adjusted basic example

--- src/Examples/Basic/RabbitMQDemo.Basic.Publisher/Program.cs
+++ src/Examples/Basic/RabbitMQDemo.Basic.Publisher/Program.cs
@@ -1,63 +1,48 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Text;
-using RabbitMQ.Client;
 
-namespace RabbitMQDemo.Publisher
+namespace RabbitMQDemo.Basic.Publisher
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var connectionHelper = new RabbitMqConnectionHelper(3);
-
-            try
+            var connectionFactory = new ConnectionFactory()
             {
-                if (!connectionHelper.TryConnect())
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                while (true)
                 {
-                    Console.WriteLine("No RabbitMQ connections are available to perform this action");
-                    Environment.Exit(0);
+                    channel.QueueDeclare(
+                        queue: "tests",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    Console.WriteLine("Type your message");
+
+                    var teste = Console.ReadLine();
+
+                    string message =
+                        $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - " +
+                        $"Message content: {teste}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "tests",
+                        basicProperties: null,
+                        body: body);
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An irrecoverable error has ocurred while trying to connect to RabbitMQ. Exception: {ex.Message}.");
-                Environment.Exit(0);
-            }
-
-            try
-            {
-                using (var channel = connectionHelper.Connection.CreateModel())
-                {
-                    while (true)
-                    {
-                        channel.QueueDeclare(
-                            queue: "tests",
-                            durable: false,
-                            exclusive: false,
-                            autoDelete: false,
-                            arguments: null);
-
-                        Console.WriteLine("Type your message");
-
-                        var teste = Console.ReadLine();
-
-                        string message =
-                            $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - " +
-                            $"Message content: {teste}";
-                        var body = Encoding.UTF8.GetBytes(message);
-
-                        channel.BasicPublish(
-                            exchange: "",
-                            routingKey: "tests",
-                            basicProperties: null,
-                            body: body);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An irrecoverable error has ocurred while trying to connect to RabbitMQ. Exception: {ex.Message}.");
-                Environment.Exit(0);
             }
         }
     }
